2c94f646322bcd6078ca4b2f68e95d39ad7d8b77
jc972058 <talia.havenaar@my.jcu.edu.au>
2025-09-28 22:40:10 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -4,9 +4,23 @@ get password
 while variable is too short keep on asking for password
 print asterixes as long as the password
 """
-length_variable = 5
-password = input("Password: ")
-while len(password) < 5:
-    print("Your password must contain 5 or more characters")
+
+def main():
+    length_variable = 5
+    password = get_password()
+    print_asterisk(password)
+
+
+def print_asterisk(password: str):
+    print("*" * len(password))
+
+
+def get_password() -> str:
     password = input("Password: ")
-print("*" * len(password))
+    while len(password) < 5:
+        print("Your password must contain 5 or more characters")
+        password = input("Password: ")
+    return password
+
+
+main()
