6b93fe30fb81cd59064b8de312774c019aef7fdd
jc972058 <talia.havenaar@my.jcu.edu.au>
2025-11-10 13:41:35 +1000

    Commit name text file Changes to #3 in files.py and finish #4

--- prac_03/files.py
+++ prac_03/files.py
@@ -8,7 +8,23 @@ out_file.close()
 #2.
 FILENAME = "name.txt"
 in_file = open(FILENAME, "r")
-for line in in_file:
-    name_in_file = line
+name_in_file = in_file.read()
 print(f"Hello {name_in_file}")
 in_file.close()
+
+#3.
+NUMBERFILE = "numbers.txt"
+with open(NUMBERFILE, "r") as number_file:
+    number1 = int(number_file.readline())
+    number2 = int(number_file.readline())
+    addition_result = number1 + number2
+    print(addition_result)
+
+#4.
+with open(NUMBERFILE, "r") as number_file:
+    result = 0
+    for line in number_file:
+        number = int(line)
+        result += number
+    print(result)
+
