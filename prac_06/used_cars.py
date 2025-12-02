f2be19c9e4daceb1410a9f9362958deeb28ceb76
jc972058 <talia.havenaar@my.jcu.edu.au>
2025-12-02 12:48:52 +1000

    edit so that car.py takes names too

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -9,8 +9,8 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
-    limo = Car(100)
+    my_car = Car("car",180)
+    limo = Car("limo", 100)
     limo.add_fuel(20)
     my_car.drive(30)
     limo.drive(115)
